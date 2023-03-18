9c098a142bce1cb03a518a6f3e0d1b79069dceae
_____W_somnium____ <56722571+HideTheTime@users.noreply.github.com>
2023-03-18 22:53:50 +0800

    update test index.js

---
+++ browser/browser/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="jquery" />
+export interface WJQuery extends JQueryStatic {
+    mytest: string;
+}
+declare let _: WJQuery;
+export default _;
+//# sourceMappingURL=index.d.ts.map
